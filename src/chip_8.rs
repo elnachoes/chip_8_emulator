05d3934c54d7aa6d7aa795cbcb7bdea87471f7d1
elnachoes <corbin.martin@protonmail.com>
2022-09-03 13:43:41 -0700

    added in multiple more instructions and implemented them including clear screen, jump, set register vx, add to register vx, and set index register

--- src/chip_8.rs
+++ src/chip_8.rs
@@ -1,6 +1,8 @@
 
 // blog used : https://tobiasvl.github.io/blog/write-a-chip-8-emulator/
 
+use core::num;
+
 pub struct Chip8 {
     // memory for the chip8 should be 4k
     pub memory : Vec<u8>,
@@ -12,8 +14,10 @@ pub struct Chip8 {
     // the specs given don't say how many stack entries there should be but I put 16
     pub stack : Vec<u16>,
     
-    // 16 bit registers for program counter and memory index register
-    pub program_counter_register : u16,
+    // 32 bit register for program counter
+    pub program_counter_register : u32,
+
+    // 16 bit register for memory index register
     pub index_register : u16,
 
     // 8 bit registers
@@ -28,10 +32,13 @@ pub struct Chip8 {
 }
 
 impl Chip8 {
+    const SCREEN_HEIGHT : usize = 32;
+    const SCREEN_WIDTH : usize = 64;
+
     pub fn new() -> Chip8 {
         Chip8 {
             memory : vec![0; 4096],
-            display : vec![vec![false; 32]; 64],
+            display : vec![vec![true; Self::SCREEN_WIDTH]; Self::SCREEN_HEIGHT],
             stack : vec![0; 16],
             program_counter_register : 0,
             index_register : 0,
@@ -42,34 +49,76 @@ impl Chip8 {
         }
     }
 
-    pub fn decode(&mut self, instruction : u32) {
-        const LAST_3_NIBBLES_BITMASK : u32 = 0xF000;
-        const FIRST_NIBBLE_BITMASK : u32 = 0x0FFF;
+    pub fn decode(&mut self, instruction : u16) {
+        const FXXX_BITMASK : u16 = 0xF000;
+        const XFFF_BITMASK : u16 = 0x0FFF;
+        const XFXX_BITMASK : u16 = 0x0F00;
+        const XXFF_BITMASK : u16 = 0x00FF;
 
         match instruction {
-            // 00E0 (clear screen) 
-            0x00E0 => {
-                print!("")
+            // 0x00E0 (clear screen) 
+            0x00E0 => { self.clear_screen_instruction() }
+
+            // 0x1NNN (jumps the program counter to a specific location)
+            i if i & FXXX_BITMASK == 0x1000 => {
+                // call the jump instruction with NNN from 1NNN to tell the program counter where to jump
+                let location = i & XFFF_BITMASK;
+                self.jump_instruction(location)
             }
 
-            // jumps the program counter to a specific location
-            i if i & LAST_3_NIBBLES_BITMASK == 0x1000 => {
-                print!("")
+            // 0x6XNN(set register VX)
+            i if i & FXXX_BITMASK == 0x6000 => {
+                //
+                let register = (i & XFXX_BITMASK) >> 8;
+                let number = i & XXFF_BITMASK;
+                self.set_register_vx_instruction(register, number);
             }
 
-            // return
-            0x00EE => {
-                print!("")
+            // add value to register vx0
+            i if i & FXXX_BITMASK == 0x7000 => {
+                //
+                let register = (i & XFXX_BITMASK) >> 8;
+                let number = i & XXFF_BITMASK;
+                self.add_register_vx_instruction(register, number);
             }
 
-            // call
-            i if i & LAST_3_NIBBLES_BITMASK == 0x2000 => {}
-
-            // call
-            i if i & LAST_3_NIBBLES_BITMASK == 0x3000 => {}
-
+            // set index register i
+            i if i & FXXX_BITMASK == 0xA000 => {
+                let number = i & XFFF_BITMASK;
+                self.set_index_register_instruction(number)
+            }
+            
+            // draw/display 
+            //TODO FIX THIS
+            i if i & FXXX_BITMASK == 0xD000 => { self.draw_display_instruction() }
 
             _ => {} 
         }
+    }
+
+    // this will set every byte storing info for the display to off
+    fn clear_screen_instruction(&mut self) {
+        self.display = vec![vec![false; Self::SCREEN_WIDTH]; Self::SCREEN_HEIGHT];
+    }
+
+    // this will just set the program counter to a specific location in program memory of NNN
+    fn jump_instruction(&mut self, location : u16) {
+        self.program_counter_register = location as u32
+    }
+
+    fn set_register_vx_instruction(&mut self, register : u16, number : u16) {
+        self.general_purpose_registers[register as usize] = number as u8
+    }
+
+    fn add_register_vx_instruction(&mut self, register : u16, number : u16) {
+        self.general_purpose_registers[register as usize] += number as u8
+    }
+
+    fn set_index_register_instruction(&mut self, number : u16) {
+        self.index_register = number;
+    }
+
+    fn draw_display_instruction(&self) {
+        // TODO : implement this later but i would update it to draw to an actual window
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,14 @@ use chip_8_emulator::Chip8;
 
 fn main() {
     let mut x = Chip8::new();
-    x.decode(0x1010);
+    print!("");
+    x.decode(0x66aa);
+    print!("");
+    x.decode(0x7601);
+    print!("");
+    x.decode(0x1111);
+    print!("");
+    
 
     // print!("{}", 0x1010 & 0xF000);
 }
