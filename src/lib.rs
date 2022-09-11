f1e08516a1c2142eb421acecc19678faefeb649b
elnachoes <corbin.martin@protonmail.com>
2022-09-10 23:06:17 -0700

    got rid of the old chip 8 windows

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,5 @@ pub use chip_8::Chip8;
 pub mod binary_op;
 pub use binary_op::BinaryOp;
 
-pub mod old_chip_8_window;
-pub use old_chip_8_window::OldChip8Window;
-
 pub mod chip_8_window;
 pub use chip_8_window::Chip8Window;
--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,5 @@ use chip_8_emulator::{Chip8};
 fn main() {
     let mut x = Chip8::new();
     x.load_rom_from_radix(&String::from("C:\\Sudo Desktop\\programming\\RustStuffs\\chip_8_emulator\\testroms\\treasure_chest.chip8"));
-    
-    println!("");
-    
-    
     x.start_processor_loop();
 }
